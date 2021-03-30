718711e693ebcf19aaba31fffd79e8f28b8b32e1
Lorenz Marqueses <marqueseslorenz@gmail.com>
2021-03-30 17:44:59 +0800

    Fabric.js test

--- js/card_art.js
+++ js/card_art.js
@@ -1,37 +1,48 @@
-function loadImages(sources, callback) {
-  var images = {};
-  var loadedImages = 0;
-  var numImages = 0;
-        // get num of sources
-  for(var src in sources) {
-    numImages++;
-  }
-  for(var src in sources) {
-    images[src] = new Image();
-    images[src].onload = function() {
-      if(++loadedImages >= numImages) {
-        callback(images);
-      }
-    };
-    images[src].src = sources[src];
+window.onload = function() {
+  window.loadedImages = [];
+  const images = [
+    "img/generator/layout/textbox_follower/0.png",
+    "img/generator/layout/textbox_follower/1.png",
+    "img/generator/layout/textbox_follower/2.png",
+    "img/generator/layout/textbox_follower/3.png",
+    "img/ui/background_Track.png"
+  ];
+  for (var i = 0; i < images.length; i++) {
+    loadedImages[i] = new Image;
+    loadedImages[i].src = images[i];
   }
 }
 
 function generate() {
-  var canvas = document.getElementById('myCanvas');
-  var ctx = canvas.getContext('2d');
-  var sources = {
-    textbox: 'img/generator/layout/textbox_follower.png',
-    border: 'img/generator/layout/border.png'
-  };
-  loadImages(sources, function(images) {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, 1920, 1120);
-    console.log(images)
-    console.log(images.textbox)
-    console.log(images.border)
-    ctx.drawImage(images.textbox, 0, 0);
-    ctx.drawImage(images.border, 0, 0);
-    download(canvas.toDataURL('image/png', 1.0), "card", "image/png");
+  var canvas = new fabric.StaticCanvas("cardTemplate", {
+    width: 1920,
+    height: 1120,
+    backgroundImage: new fabric.Image(loadedImages[4]),
+    // ...
   });
+  drawTextBox(0, canvas);
+  download(canvas.toDataURL('image/png', 1.0), "card", "image/png");
+}
+
+function drawTextBox(type, canvas) {
+  var lineDrawPositionY;
+  if (type == 0) {
+    canvas.add(new fabric.Image(loadedImages[0], {top: 240, left: 753}));
+
+    for (var i = 0; i < 4; i++) {
+      canvas.add(new fabric.Image(loadedImages[1], {top: 394+(40*i), left: 753}));
+      lineDrawPositionY = 394+(40*i);
+    };
+
+    canvas.add(new fabric.Image(loadedImages[2], {top: lineDrawPositionY+40, left: 753}));
+    lineDrawPositionY += 197;
+
+    for (var i = 0; i < 4; i++) {
+      canvas.add(new fabric.Image(loadedImages[1], {top: lineDrawPositionY, left: 753}));
+      lineDrawPositionY += 40;
+    };
+
+    canvas.add(new fabric.Image(loadedImages[3], {top: lineDrawPositionY, left: 753}));
+  };
+  canvas.requestRenderAll;
 }
